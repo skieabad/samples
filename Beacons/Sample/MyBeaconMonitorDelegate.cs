0d6c959877858e20026a05d29b96bddc6c0c3368
Allan Ritchie <allan.ritchie@gmail.com>
2021-09-29 00:29:10 -0400

    wip

--- Beacons/Sample/MyBeaconMonitorDelegate.cs
+++ Beacons/Sample/MyBeaconMonitorDelegate.cs
@@ -9,11 +9,11 @@ namespace Sample
     {
         readonly SampleSqliteConnection conn;
         public MyBeaconMonitorDelegate(SampleSqliteConnection conn) => this.conn = conn;
-
-
-        public Task OnStatusChanged(BeaconRegionState newStatus, BeaconRegion region)
+        public Task OnStatusChanged(BeaconRegionState newStatus, BeaconRegion region) => this.conn.InsertAsync(new ShinyEvent
         {
-            throw new NotImplementedException();
-        }
+            Text = $"{region.Identifier} was {newStatus.ToString().ToLower()}",
+            Detail = $"UUID: {region.Uuid} - M: {region.Major} - m: {region.Minor}",
+            Timestamp = DateTime.UtcNow
+        });
     }
 }
