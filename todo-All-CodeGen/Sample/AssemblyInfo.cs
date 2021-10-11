8bd8015e95ce3a48392bc3f93f04a4e7db5cf6a3
Allan Ritchie <allan.ritchie@gmail.com>
2021-10-11 01:19:03 -0400

    updates

--- todo-All-CodeGen/Sample/AssemblyInfo.cs
+++ todo-All-CodeGen/Sample/AssemblyInfo.cs
@@ -1,6 +1,4 @@
 using Xamarin.Forms.Xaml;
 
-//[assembly: Shiny.ShinyServiceAttribute()]
-
 [assembly: Shiny.GenerateStaticClasses("Sample")]
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
