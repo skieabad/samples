17c8919aa45a3c118e50bd32cf7f4cbefa113871
Allan Ritchie <allan.ritchie@gmail.com>
2021-10-18 23:27:41 -0400

    Call directory updated

---
+++ Sponsors-CallDirectory/Sample.iOS.CallDirectoryExtension/CallDirectoryHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Foundation;
+
+
+namespace Sample.iOS.CallDirectoryExtension
+{
+    [Register("CallDirectoryHandler")]
+    public class CallDirectoryHandler : Shiny.CallDirectory.ShinyCallDirectoryExtension
+    {
+        public CallDirectoryHandler(IntPtr handle) : base(handle)
+        {
+        }
+    }
+}
---
+++ Sponsors-CallDirectory/Sample/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Sample
+{
+    public partial class App : Shiny.FrameworkApplication
+    {
+        protected override void Initialize()
+        {
+            this.InitializeComponent();
+            base.Initialize();
+        }
+    }
+}
