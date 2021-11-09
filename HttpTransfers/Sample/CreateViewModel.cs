fafdc7124fece907debd5059980252d2a0f52055
Allan Ritchie <allan.ritchie@gmail.com>
2021-11-09 10:19:00 -0500

    Update samples

--- HttpTransfers/Sample/CreateViewModel.cs
+++ HttpTransfers/Sample/CreateViewModel.cs
@@ -1,10 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Windows.Input;
 using Shiny;
 using Shiny.Net.Http;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 
@@ -12,39 +13,33 @@ namespace Sample
 {
     public class CreateViewModel : SampleViewModel
     {
+        const string RANDOM_FILE_NAME = "upload.random";
+        readonly IPlatform platform;
         IDisposable? sub;
 
 
         public CreateViewModel()
         {
-            var platform = ShinyHost.Resolve<IPlatform>();
+            this.Url = "https://acrmonster:44378/upload";
+
+            this.platform = ShinyHost.Resolve<IPlatform>();
             var httpTransfers = ShinyHost.Resolve<IHttpTransferManager>();
 
-            this.ManageUploads = new Command(
-                async () => this.Navigation.PushAsync(new ManageUploadsPage())
-            );
 
             this.SelectUpload = new Command(async () =>
             {
-                var files = platform.AppData.GetFiles("upload.*", SearchOption.TopDirectoryOnly);
-                if (!files.Any())
+                var result = await FilePicker.PickAsync(new PickOptions
                 {
-                    await this.Alert("There are not files to upload.  Use 'Manage Uploads' below to create them");
-                }
-                else
-                {
-                    var cfg = new Dictionary<string, Action>();
-                    foreach (var file in files)
-                        cfg.Add(file.Name, () => this.FileName = file.Name);
-
-                    //await dialogs.ActionSheet("Actions", cfg);
-                }
+                    PickerTitle = "Select a file to upload"
+                });
+                if (result != null)
+                    this.FilePath = result.FullPath;
             });
 
-            this.Save = new Command(async () =>
+            this.Save = this.LoadingCommand(async () =>
             {
                 this.ErrorMessage = "";
-                if (this.FileName.IsEmpty())
+                if (this.FilePath.IsEmpty())
                 {
                     this.ErrorMessage = "Enter a filename";
                     return;
@@ -54,14 +49,29 @@ namespace Sample
                     this.ErrorMessage = "Please enter a valid URI";
                     return;
                 }
-                var path = Path.Combine(platform.AppData.FullName, this.FileName);
-                var request = new HttpTransferRequest(this.Url, path, this.IsUpload)
+                if (this.IsUpload && !File.Exists(this.FilePath))
+                {
+                    await this.Alert("This file does not exist");
+                    return;
+                }
+                var request = new HttpTransferRequest(this.Url, this.FilePath, this.IsUpload)
                 {
                     UseMeteredConnection = this.UseMeteredConnection
                 };
                 await httpTransfers.Enqueue(request);
 
                 await this.Navigation.PopAsync();
+            });
+
+            this.CreateRandom = this.LoadingCommand(async () =>
+            {
+                if (this.SizeInMegabytes <= 0)
+                {
+                    await this.Alert("Invalid File Size");
+                    return;
+                }
+                await this.GenerateRandom();
+                this.FilePath = this.GetRandomFilePath();
             });
         }
 
@@ -71,9 +81,16 @@ namespace Sample
             base.OnAppearing();
             this.sub = this.WhenAnyProperty(x => x.IsUpload).Subscribe(upload =>
             {
-                if (!upload && this.FileName.IsEmpty())
-                    this.FileName = Guid.NewGuid().ToString();
-
+                if (!upload)
+                {
+                    this.FilePath = Path.Combine(this.platform.AppData.FullName, Guid.NewGuid().ToString() + ".download");
+                }
+                else
+                {
+                    var path = this.GetRandomFilePath();
+                    if (File.Exists(path))
+                        this.FilePath = path;
+                }
                 this.Title = upload ? "New Upload" : "New Download";
             });
         }
@@ -88,7 +105,8 @@ namespace Sample
 
         public ICommand Save { get; }
         public ICommand SelectUpload { get; }
-        public ICommand ManageUploads { get; }
+        public ICommand Delete { get; }
+        public ICommand CreateRandom { get; }
 
 
         string title;
@@ -115,7 +133,7 @@ namespace Sample
         }
 
 
-        bool meterConn;
+        bool meterConn = true;
         public bool UseMeteredConnection
         {
             get => this.meterConn;
@@ -131,11 +149,42 @@ namespace Sample
         }
 
 
-        string fileName;
-        public string FileName
+        string filePath;
+        public string FilePath
         {
-            get => this.fileName;
-            set => this.Set(ref this.fileName, value);
+            get => this.filePath;
+            private set => this.Set(ref this.filePath, value);
         }
+
+
+        int fsize = 100;
+        public int SizeInMegabytes
+        {
+            get => this.fsize;
+            set => this.Set(ref this.fsize, value);
+        }
+
+
+        string GetRandomFilePath() => Path.Combine(this.platform.AppData.FullName, RANDOM_FILE_NAME);
+        Task GenerateRandom() => Task.Run(() =>
+        {
+            var path = this.GetRandomFilePath();
+            if (File.Exists(path))
+                File.Delete(path); // delete previous random file
+
+            var byteSize = this.SizeInMegabytes * 1024 * 1024;
+            var data = new byte[8192];
+            var rng = new Random();
+
+            using (var fs = new FileStream(path, FileMode.Create))
+            {
+                while (fs.Length < byteSize)
+                {
+                    rng.NextBytes(data);
+                    fs.Write(data, 0, data.Length);
+                    fs.Flush();
+                }
+            }
+        });
     }
 }
