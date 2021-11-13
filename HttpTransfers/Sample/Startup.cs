a8c3f0f74ae641772ae558d8a99c0e61f168514e
Allan Ritchie <allan.ritchie@gmail.com>
2021-11-12 21:43:32 -0500

    Update Startup.cs

--- HttpTransfers/Sample/Startup.cs
+++ HttpTransfers/Sample/Startup.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Shiny;
 
 
@@ -6,6 +7,13 @@ namespace Sample
 {
     public class Startup : ShinyStartup
     {
+        public override void ConfigureLogging(ILoggingBuilder builder, IPlatform platform)
+        {
+            base.ConfigureLogging(builder, platform);
+            builder.SetMinimumLevel(LogLevel.Debug);
+        }
+
+
         public override void ConfigureServices(IServiceCollection services, IPlatform platform)
         {
             // we inject our db so we can use it in our shiny background events to store them for display later
