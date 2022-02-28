1d5b8366d60ee8db57749527ab9777a98e44e4b7
Allan Ritchie <allan.ritchie@gmail.com>
2022-02-28 12:44:23 -0500

    add test case

--- BluetoothLE-Functions/Sample/MainViewModel.cs
+++ BluetoothLE-Functions/Sample/MainViewModel.cs
@@ -3,9 +3,12 @@ using Shiny;
 using Shiny.BluetoothLE;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reactive.Linq;
 using System.Reactive.Threading.Tasks;
+using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 
 namespace Sample
@@ -16,18 +19,38 @@ namespace Sample
         {
             this.Functions = new List<CommandItem>
             {
-                Create("GetAllCharacteristicAsync", cmd => DoPeripheral(bleManager, cmd, async peripheral =>
-                {
-                    var chs = await peripheral.GetAllCharacteristicsAsync();
-                    cmd.Detail = $"Found {chs.Count} Characteristics";
-                }))
-            };
-        }
+                Create(
+                    "GetAllCharacteristicAsync",
+                    cmd => DoPeripheral(bleManager, cmd, async peripheral =>
+                    {
+                        var chs = await peripheral.GetAllCharacteristicsAsync();
+                        cmd.Detail = $"Found {chs.Count} Characteristics";
 
+                        chs.FirstOrDefault(x => x.Uuid.Equals("FFF1", StringComparison.InvariantCultureIgnoreCase)).Notify().Subscribe(x =>
+                        {
+                            cmd.Detail = Convert.ToBase64String(x.Data);
+                        });
+                        await chs.FirstOrDefault(x => x.Uuid.Equals("FFF2", StringComparison.InvariantCultureIgnoreCase)).WriteAsync(Encoding.ASCII.GetBytes("ATZ\n"), true);
+                    })
+                ),
+                Create(
+                    "ReadRssi",
+                    cmd => DoPeripheral(bleManager, cmd, async peripheral =>
+                    {
+                        var rssi = await peripheral.ReadRssi();
+                        cmd.Detail = "RSSI is " + rssi;
+                    })
+                )
+            };
+
+            this.Test = this.Functions[0].PrimaryCommand;
+        }
 
 
         public List<CommandItem> Functions { get; }
 
+
+        public ICommand Test { get; }
 
         async Task DoPeripheral(IBleManager bleManager, CommandItem cmd, Func<IPeripheral, Task> doWork)
         {
@@ -40,6 +63,7 @@ namespace Sample
                     .Timeout(TimeSpan.FromSeconds(20))
                     .ToTask();
 
+                await peripheral.ConnectAsync();
                 await doWork(peripheral).ConfigureAwait(false);
             }
             catch
