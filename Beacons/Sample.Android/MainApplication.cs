998e47304e24f6f33e737ccea2d5042217fcadb0
Allan Ritchie <allan.ritchie@gmail.com>
2022-06-04 22:06:09 -0400

    WIP - bring beacons up to newest version

---
+++ Beacons/Sample.Android/MainApplication.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Shiny;
+using Android.App;
+using Android.Runtime;
+
+
+namespace Sample.Droid
+{
+	[Application]
+	public class MainApplication : Application
+	{
+		public MainApplication(IntPtr handle, JniHandleOwnership transfer) : base(handle, transfer) { }
+
+		public override void OnCreate()
+		{
+			this.ShinyOnCreate(new Startup());
+			base.OnCreate();
+		}
+	}
+}
