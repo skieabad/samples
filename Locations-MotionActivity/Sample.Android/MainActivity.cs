0138655ebc8501486a984bd52dbd0927803e57c9
Allan Ritchie <allan.ritchie@gmail.com>
2022-07-01 10:31:37 -0400

    Update product structure for later

--- Locations-MotionActivity/Sample.Android/MainActivity.cs
+++ Locations-MotionActivity/Sample.Android/MainActivity.cs
@@ -1,17 +1,17 @@
 ﻿using System;
 using Android.App;
 using Android.Content.PM;
+using Android.OS;
+using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
+using Shiny;
+using Android.Content;
 
-[assembly: Shiny.ShinyApplication(
-    ShinyStartupTypeName = "Sample.Startup",
-    XamarinFormsAppTypeName = "Sample.App"
-)]
 
 namespace Sample.Droid
 {
     [Activity(
-        Label = "Sample",
+        Label = "GPS",
         Icon = "@mipmap/icon",
         Theme = "@style/MainTheme",
         MainLauncher = true,
@@ -22,7 +22,36 @@ namespace Sample.Droid
             ConfigChanges.ScreenLayout |
             ConfigChanges.SmallestScreenSize
     )]
-    public partial class MainActivity : FormsAppCompatActivity
+    public class MainActivity : FormsAppCompatActivity
     {
-    }
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			TabLayoutResource = Resource.Layout.Tabbar;
+			ToolbarResource = Resource.Layout.Toolbar;
+			base.OnCreate(savedInstanceState);
+			Forms.Init(this, savedInstanceState);
+			this.LoadApplication(new App());
+		}
+
+
+		protected override void OnNewIntent(Intent intent)
+		{
+			base.OnNewIntent(intent);
+			this.ShinyOnNewIntent(intent);
+		}
+
+
+		protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+		{
+			base.OnActivityResult(requestCode, resultCode, data);
+			this.ShinyOnActivityResult(requestCode, resultCode, data);
+		}
+
+
+		public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
+		{
+			base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+			this.ShinyOnRequestPermissionsResult(requestCode, permissions, grantResults);
+		}
+	}
 }
--- Locations-MotionActivity/Sample/AssemblyInfo.cs
+++ Locations-MotionActivity/Sample/AssemblyInfo.cs
@@ -1,4 +1,3 @@
 using Xamarin.Forms.Xaml;
 
-[assembly: Shiny.GenerateStaticClasses("Sample")]
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
